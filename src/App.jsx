7610f9ebfd7cdd533b48cbf4987f6d49d5548c3f
Tonyah007 <osykins10@gmail.com>
2023-06-04 06:11:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import moviesList from "./movies";
+
+function App() {
+
+    const [movies, setMovies] = useState(moviesList);
+    const [results, setResults] = useState([]);
+    const [showSearch, setShowSearch] = useState(false);
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+  
+      const form = e.target;
+  
+      const movie = {
+        title: form.title.value,
+        description: form.description.value,
+        posterUrl: form.posterUrl.value,
+        rating: parseInt(form.rating.value),
+      };
+  
+      setMovies((prev) => [...prev, movie]);
+    }
+  
+    function handleSearch(e) {
+      e.preventDefault();
+      const form = e.target;
+      const searchKeyword = form.search.value;
+  
+      if (searchKeyword === "") {
+        setShowSearch(false);
+        return;
+      }
+  
+      const filteredMovies = movies.filter((movie) => {
+        return (
+          movie.title.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+          movie.rating == searchKeyword
+        );
+      });
+  
+      setResults(() => filteredMovies);
+      setShowSearch(true);
+    }
+  
+    return (
+      <div className="app">
+        <aside>
+          <h1>Add a new movie</h1>
+          <form onSubmit={handleSubmit}>
+            <input name="title" type="text" placeholder="Movie title" required />
+            <textarea
+              name="description"
+              type="text"
+              rows={5}
+              placeholder="Movie description"
+              required
+            ></textarea>
+            <input
+              name="posterUrl"
+              type="text"
+              placeholder="Movie poster url"
+              required
+            />
+            <input
+              name="rating"
+              type="number"
+              placeholder="Movie rating"
+              required
+            />
+            <button type="submit">Add movie</button>
+          </form>
+        </aside>
+  
+        <main>
+          <form onSubmit={handleSearch}>
+            <input name="search" type="text" placeholder="Search" />
+          </form>
+  
+          {!showSearch && <movieList movies={movies} />}
+          {showSearch && <movieList movies={results} />}
+        </main>
+      </div>
+    );
+  
+  }
+
+export default App;
---
+++ src/movies.js
@@ -0,0 +1,31 @@
+const movies = [
+    {
+    title: "Bubble",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur non, beatae nisi temporibus aliquam.",
+    posterUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-5iDcudeb9_QSnefadhhacLQbWdNLf4-7lg&usqp=CAU",
+    rating: 10,
+    },
+
+    {
+    title: "Naruto The Movie",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur non, beatae nisi temporibus aliquam.",
+    posterUrl: "https://www.anime-planet.com/images/anime/screenshots/bubble-18873-1.jpg",
+    rating: 10,
+    },
+    
+    {
+    title: "Avengers",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur non, beatae nisi temporibus aliquam.",
+    posterUrl: "https://www.anime-planet.com/images/anime/screenshots/bubble-18873-1.jpg",
+    rating: 10,
+    },
+    
+    {
+    title: "Black Panther",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consequatur non, beatae nisi temporibus aliquam.",
+    posterUrl: "https://www.anime-planet.com/images/anime/screenshots/bubble-18873-1.jpg",
+    rating: 10,
+    },
+    ];
+
+export default movies;
